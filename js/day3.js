f34641cc5407a7df37f91831aac0830505fd26b5
Dallas S. <andrewdallassherman@gmail.com>
2024-12-13 21:29:08 +0900

    day3 1 and 2 done again

--- js/day3.js
+++ js/day3.js
@@ -1,8 +1,51 @@
-fs = require("fs");
+const fs = require("fs");
+const path = require("path");
 
-fs.readFile("./input/day3.txt", "utf-8", (err, data) => {
-  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
-  const num = data.match(regex);
+const input = fs.readFileSync(
+  path.resolve(__dirname, "./input/day3.txt"),
+  "utf8",
+);
+const lines = input
+  .replace(/\r/g, "")
+  .split("\n")
+  .filter((x) => x != "")
+  .map((x) => x.matchAll(/mul\((\d+),(\d+)\)/g));
 
-  console.log(num);
-});
+// part 1
+
+const total = lines.reduce((a, x) => {
+  let result = 0;
+  for (let y of x) {
+    result += y[1] * y[2];
+  }
+
+  return a + result;
+}, 0);
+
+console.log(total);
+
+//part 2
+const linesTwo = input
+  .replace(/\r/g, "")
+  .split("\n")
+  .filter((x) => x != "")
+  .map((x) => x.matchAll(/(?:mul\((\d+),(\d+)\))|(?:do\(\))|(?:don't\(\))/g));
+
+let enabled = true;
+let doDont = linesTwo.reduce((a, x) => {
+  if (!x) return a;
+
+  let result = 0;
+  for (let y of x) {
+    if (y[0] == "don't()") enabled = false;
+    else if (y[0] == "do()") enabled = true;
+    else if (enabled) result += y[1] * y[2];
+  }
+
+  return a + result;
+}, 0);
+
+console.log(doDont);
+
+// again had to use some online help and use some stuff that others wrote, not claiming this as my own.
+// kind of a cool regex challenge now that i see how to do the code!
