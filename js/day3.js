466e1b4a8c77e2563bf9f0950505c86ffac7532e
Dallas S. <andrewdallassherman@gmail.com>
2024-12-13 14:09:05 +0900

    started day 3, need to figure out how to combine my mul numbers...

--- js/day3.js
+++ js/day3.js
@@ -1,7 +1,8 @@
-fs.readFile("./input/day2.txt", "utf-8", (err, data) => {
-  const lines = data
-    .replace(/\r/g, "")
-    .split("\n")
-    .filter((x) => x != "")
-    .map((x) => x.split(" ").map(Number));
+fs = require("fs");
+
+fs.readFile("./input/day3.txt", "utf-8", (err, data) => {
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+  const num = data.match(regex);
+
+  console.log(num);
 });
