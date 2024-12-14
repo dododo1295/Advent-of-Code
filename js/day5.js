ba3832f395ac9e742ab130af22545a8346e5285a
Dallas S. <andrewdallassherman@gmail.com>
2024-12-14 22:35:39 +0900

    outputting NaN and can't figure out why...

--- js/day5.js
+++ js/day5.js
@@ -1,6 +1,29 @@
 const fs = require("fs");
-const path = require("path");
-const input = fs
-  .readFileSync(path.resolve(__dirname, "./input/day4.txt"), "utf8")
-  .replace(/\r/g, "")
-  .split("\n\n");
+
+const isOrdered = (update, rules) => {
+  for (let i = 0; i < update.length - 1; i++) {
+    if (
+      !rules.find((rule) => rule[0] === update[i] && rule[1] === update[i + 1])
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+fs.readFile("./input/day5.txt", "utf-8", (err, data) => {
+  let [rules, updates] = data.split("\n\n");
+
+  rules = rules.split("\n").map((rule) => rule.split("|"));
+  updates = updates.split("\n").map((x) => x.split(","));
+
+  // Part 1
+  const part1 = updates.reduce((acc, update) => {
+    if (!isOrdered(update, rules)) return acc;
+
+    const midIndex = Math.floor(update.length / 2);
+    const mid = parseInt(update[midIndex]);
+    acc += mid;
+    return acc;
+  }, 0);
+  console.log(part1);
+});
