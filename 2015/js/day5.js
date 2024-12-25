27ab02e3e5a17812be81762a27b52e4f518b9398
Dallas S. <andrewdallassherman@gmail.com>
2024-12-25 21:19:17 +0900

    setting up day 5

---
+++ 2015/js/day5.js
@@ -0,0 +1,8 @@
+const strings = Deno.readTextFileSync("./input/day5.txt").trimEnd().split("\n");
+
+console.log(strings);
+
+// reread the website dude....
+//
+// I'm assuming i need to forEach string of strings and then some sort of regex BUT
+// that's from the first read so I'll check in tomorrow again
