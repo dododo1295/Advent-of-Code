06f4f4761291129c38d6d3680724b68222c16b7a
Dallas S. <andrewdallassherman@gmail.com>
2024-12-25 12:47:23 +0900

    finished day 1 (no tutorials!!!!)

---
+++ 2023/js/day1.js
@@ -0,0 +1,34 @@
+const map = Deno.readTextFileSync("./input/day1.txt").trimEnd().split("");
+
+// part 1
+
+let floor = 0;
+
+for (let i = 0; i < map.length; i++) {
+  if (map[i] === "(") {
+    floor++;
+  } else if (map[i] === ")") {
+    floor--;
+  }
+}
+
+console.log("The map leads to floor: ", floor);
+
+//part part 2
+
+floor = 0;
+let step = 0;
+
+for (let i = 0; i < map.length; i++) {
+  if (map[i] === "(") {
+    floor++;
+  } else if (map[i] === ")") {
+    floor--;
+    if (floor === -1) {
+      step = i + 1;
+      break;
+    }
+  }
+}
+
+console.log(step);
