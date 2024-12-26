c891c83933c5a2a39d4722b7687520388a751cdf
Dallas S. <andrewdallassherman@gmail.com>
2024-12-26 10:38:02 +0900

    finish day 5... regex....I hate them

--- 2015/js/day5.js
+++ 2015/js/day5.js
@@ -1,8 +1,32 @@
-const strings = Deno.readTextFileSync("./input/day5.txt").trimEnd().split("\n");
+const strings = Deno.readTextFileSync("./input/day5.txt").trim().split("\n");
 
-console.log(strings);
+// part 1
+const rules = /^(?=(.*[aeiou]){3})(?=.*([a-z])\2)(?!.*(ab|cd|pq|xy)).*$/;
 
-// reread the website dude....
-//
-// I'm assuming i need to forEach string of strings and then some sort of regex BUT
-// that's from the first read so I'll check in tomorrow again
+const niceStrings = [];
+const naughtyStrings = [];
+
+for (let i = 0; i < strings.length; i++) {
+  if (rules.test(strings[i])) {
+    niceStrings.push(strings[i]);
+  } else {
+    naughtyStrings.push(strings[i]);
+  }
+}
+console.log("Nice: ", niceStrings.length);
+console.log("Naughty: ", naughtyStrings.length);
+
+// part 2 w/ help
+
+const isContainPair = (string) => /([a-z][a-z]).*\1/.test(string);
+const isContainRepeatLetter = (string) => /([a-z])[a-z]\1/.test(string);
+
+const isNiceString = (string) =>
+  !!(isContainPair(string) && isContainRepeatLetter(string));
+
+const result = strings.reduce(
+  (total, string) => isNiceString(string) ? ++total : total,
+  0,
+);
+
+console.log("Santa's new Nice List: ", result);
